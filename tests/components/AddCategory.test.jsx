8f0f49534d96f160a695d9a844db84608f7e7afc
Marcelo Puebla <marcenp23@gmail.com>
2023-06-29 16:09:57 -0300

    unit test on add category

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -10,4 +10,37 @@ describe('Pruebas en AddCategory', () => {
 
     expect(input.value).toBe('Saitama');
   });
+
+  test('debe de llamar onNewCategory si el input tiene un valor', () => {
+    const inputValue = 'Saitama';
+    const onNewCategory = jest.fn();
+
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
+
+    expect(input.value).toBe('');
+    expect(onNewCategory).toHaveBeenCalled();
+    expect(onNewCategory).toHaveBeenCalledTimes(1);
+    expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+  });
+
+  test('no debe de llamar onNewCategory si el input no tiene valor', () => {
+    const inputValue = '';
+    const onNewCategory = jest.fn();
+
+    render(<AddCategory onNewCategory={onNewCategory} />);
+
+    const input = screen.getByRole('textbox');
+    const form = screen.getByRole('form');
+
+    fireEvent.input(input, { target: { value: inputValue } });
+    fireEvent.submit(form);
+
+    expect(onNewCategory).not.toHaveBeenCalled();
+  });
 });
